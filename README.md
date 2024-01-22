9585262b8d0fde90a9ca095a427d0bde9f2a072d
Lordfiber <69179042+Lordfiber@users.noreply.github.com>
2024-01-22 21:07:46 +0800

    feat: add initial code (#3)
    * feat: using jcasbin int springSecurity
    
    feat: using jcasbin int springSecurity
    
    * feat: using jcasbin int springSecurity
    
    feat: using jcasbin int springSecurity

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# casbin-spring-security-starter
+# using casbin in SpringSecurity
+
+## inform
+    To skip the tedious and complicated login process of Spring Security, 
+    we're using its built-in login mode, which allows us to focus on the model authentication module.
+
+## DataBase 
+    For the database, we're using mysql. 
+    Remember to create a casbin database if you don't have one, and configure it in the application.yml file.
+
+## Simple Example
+    check file examples.rbac.policy.csv you can find out we are using rbac model
+    I registered with the identity of Alice who can read data1
+![img_1.png](img_1.png)
+
+    Than test the  result
+![img_2.png](img_2.png)
+
+![img_3.png](img_3.png)
---
+++ src/test/java/org/casbin/JdbcTest.java
@@ -0,0 +1,60 @@
+// Copyright 2024 The Casdoor Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.casbin;
+
+import org.casbin.adapter.JDBCAdapter;
+import org.casbin.jcasbin.main.Enforcer;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class JdbcTest {
+    @Value(value = "${mysql.url}")
+    private String url;
+    @Value(value = "${mysql.username}")
+    private String username;
+    @Value(value = "${mysql.password}")
+    private String password;
+    @Value(value = "${mysql.driver}")
+    private String driver;
+
+    @Test
+    public void TestJdbc() throws Exception {
+        //save policy to database
+        Enforcer e = new Enforcer("examples/rbac_model.conf", "examples/rbac_policy.csv");
+        JDBCAdapter adapter = new JDBCAdapter(driver, url, username, password);
+        adapter.savePolicy(e.getModel());
+
+        //read policy to database
+        e.clearPolicy();
+        adapter.loadPolicy(e.getModel());
+
+        Enforcer newOne=new Enforcer("examples/rbac_model.conf",e.getAdapter());
+        System.out.println(newOne.getPolicy());
+
+    }
+
+    @Test
+    public void TestModelAuth(){
+        Enforcer e = new Enforcer("examples/rbac_model.conf", "examples/rbac_policy.csv");
+        boolean enforce = e.enforce("alice", "data1", "read");
+        System.out.println(enforce);
+    }
+}
